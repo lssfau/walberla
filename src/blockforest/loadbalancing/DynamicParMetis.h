945d0a7371eab9541b5b8374ba2e170b6b5e1444
Sebastian Eibl <sebastian.eibl@fau.de>
2017-11-16 14:09:01 +0100

    added missing include guards

--- src/blockforest/loadbalancing/DynamicParMetis.h
+++ src/blockforest/loadbalancing/DynamicParMetis.h
@@ -19,6 +19,8 @@
 //
 //======================================================================================================================
 
+#pragma once
+
 #include "blockforest/PhantomBlockForest.h"
 
 #include "core/DataTypes.h"
--- src/blockforest/loadbalancing/StaticParMetis.h
+++ src/blockforest/loadbalancing/StaticParMetis.h
@@ -19,6 +19,8 @@
 //
 //======================================================================================================================
 
+#pragma once
+
 #include "blockforest/SetupBlockForest.h"
 
 #include "core/DataTypes.h"
