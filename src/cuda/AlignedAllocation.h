a3ca9e4a5c1dfa7dd53d330bf190c2c58292ee7d
Martin Bauer <martin.bauer@fau.de>
2018-11-14 12:19:33 +0100

    CUDA Aligned Allocation - splitted in header and cpp

--- src/cuda/AlignedAllocation.h
+++ src/cuda/AlignedAllocation.h
@@ -18,73 +18,22 @@
 //! \author Martin Bauer <martin.bauer@fau.de>
 //
 //======================================================================================================================
+#pragma once
 
-#include "cuda/ErrorChecking.h"
-#include "core/debug/CheckFunctions.h"
-#include "core/debug/Debug.h"
-#include "core/logging/Logging.h"
+#include "core/DataTypes.h"
+
 
 namespace walberla {
 namespace cuda {
 
-static std::map<void *, void*> freePointers_;
+   void *allocate_aligned_with_offset( uint_t size, uint_t alignment, uint_t offset );
 
 
-inline void *allocate_aligned_with_offset( uint_t size, uint_t alignment, uint_t offset )
-{
-   // With 0 alignment this function makes no sense
-   // use normal malloc instead
-   WALBERLA_ASSERT_GREATER( alignment, 0 );
-   // Tests if alignment is power of two (assuming alignment>0)
-   WALBERLA_ASSERT( !(alignment & (alignment - 1)) );
-
-   WALBERLA_ASSERT_LESS( offset, alignment );
-
-   if( offset == 0 )
-   {
-      void * result = nullptr;
-      WALBERLA_CUDA_CHECK( cudaMalloc( &result, size ) );
-      freePointers_[result] = result;
-      return result;
-   }
-
-   void *pa;  // pointer to allocated memory
-   void *ptr; // pointer to usable aligned memory
-
-   WALBERLA_CUDA_CHECK( cudaMalloc( &pa, size + alignment ));
-   WALBERLA_CHECK_EQUAL(size_t(pa) % alignment, 0 , "CUDA malloc did not return memory with requested alignment");
-   ptr = (void *) ((char *) (pa) + alignment - offset);
-   freePointers_[ptr] = pa;
-
-   WALBERLA_ASSERT_EQUAL(((size_t) ptr + offset) % alignment, 0 );
-   return ptr;
-}
+   void free_aligned_with_offset( void *ptr );
 
 
-inline void free_aligned_with_offset( void *ptr )
-{
-   // assume that pointer to real allocated chunk is stored just before
-   // chunk that was given to user
-   WALBERLA_CUDA_CHECK( cudaFree( freePointers_[ptr] ));
-   freePointers_.erase(ptr);
-}
-
-
-
-inline void *allocate_pitched_with_offset( size_t &pitchOut, size_t width, size_t height,
-                                           size_t alignment, size_t alignmentOffset )
-{
-   if( width % alignment == 0)
-      pitchOut = width;
-   else
-      pitchOut = ((width + alignment) / alignment ) * alignment;
-
-   WALBERLA_ASSERT_GREATER_EQUAL( pitchOut, width );
-   WALBERLA_ASSERT_EQUAL( pitchOut  % alignment, 0 );
-
-   return allocate_aligned_with_offset( pitchOut * height, alignment, alignmentOffset );
-}
+   void *allocate_pitched_with_offset( size_t &pitchOut, size_t width, size_t height,
+                                       size_t alignment, size_t alignmentOffset );
 
 } // namespace cuda
 } // namespace walberla
-
