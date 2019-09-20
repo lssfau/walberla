d01658209fde7ee34d60f252d490c5b19802e95f
Sebastian Eibl <sebastian.eibl@fau.de>
2019-09-20 11:17:54 +0200

    [ADD] mesa_pd grafana benchmark

---
+++ apps/benchmarks/GranularGas/mesa_pd_upload.py
@@ -0,0 +1,52 @@
+#! /usr/bin/env python3
+
+import os
+import time
+import math
+import random
+import re
+from influxdb import InfluxDBClient
+from git import Repo
+
+
+class Upload:
+   def __init__(self):
+      try:
+         self.write_user_pw = os.environ["INFLUXDB_MESAPD_PW"]
+      except KeyError:
+         import sys
+         print('Password for the InfluxDB write_user was not set.\n',
+               'See https://docs.gitlab.com/ee/ci/variables/#secret-variables', file=sys.stderr)
+         exc_info = sys.exc_info()
+         raise exc_info[0].with_traceback(exc_info[1], exc_info[2])
+
+      self.client = InfluxDBClient('i10grafana.informatik.uni-erlangen.de', 8086,
+                                   'mesa_pd', self.write_user_pw, 'mesa_pd')
+
+   def process(self, filename):
+      tts = dict()
+
+      with open(filename) as f:
+         for s in f.readlines():
+            m = re.search(r'\[0\]\s*(\w*)\s*\|[\s\d\.\%]*\|\s*([\d\.]*)', s)
+
+            if (m != None):
+               tts[m.group(1)] = float(m.group(2))
+
+      json_body = [
+         {
+            'measurement': 'mesa_pd_benchmark',
+            'tags': {
+                  'host'            : os.uname()[1],
+                  'image'           : os.environ["DOCKER_IMAGE_NAME"],
+            },
+            'time': int(time.time()),
+            'fields': tts
+         }
+      ]
+      print(tts)
+      self.client.write_points(json_body, time_precision='s')
+
+if __name__ == "__main__":
+   up = Upload()
+   up.process("mesa_pd.txt")
