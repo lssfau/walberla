0682889aa6ae53e1781866b9687b6692632b59a2
Christoph Schwarzmeier <christoph.schwarzmeier@fau.de>
2019-10-29 07:21:55 +0100

    Add missing header in BodyMapping.h

--- src/pe_coupling/momentum_exchange_method/BodyMapping.h
+++ src/pe_coupling/momentum_exchange_method/BodyMapping.h
@@ -1,15 +1,15 @@
 //======================================================================================================================
 //
-//  This file is part of waLBerla. waLBerla is free software: you can 
+//  This file is part of waLBerla. waLBerla is free software: you can
 //  redistribute it and/or modify it under the terms of the GNU General Public
-//  License as published by the Free Software Foundation, either version 3 of 
+//  License as published by the Free Software Foundation, either version 3 of
 //  the License, or (at your option) any later version.
-//  
-//  waLBerla is distributed in the hope that it will be useful, but WITHOUT 
-//  ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or 
-//  FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License 
+//
+//  waLBerla is distributed in the hope that it will be useful, but WITHOUT
+//  ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+//  FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
 //  for more details.
-//  
+//
 //  You should have received a copy of the GNU General Public License along
 //  with waLBerla (see COPYING.txt). If not, see <http://www.gnu.org/licenses/>.
 //
@@ -33,6 +33,7 @@
 #include "pe/rigidbody/BodyIterators.h"
 
 #include "pe_coupling/mapping/BodyBBMapping.h"
+#include "pe_coupling/momentum_exchange_method/destruction/Destroyer.h"
 #include "pe_coupling/utility/BodySelectorFunctions.h"
 
 #include <functional>
