afa2be7d927a32f329d121130efe4d1db30c1072
Michael Kuron <mkuron@icp.uni-stuttgart.de>
2019-11-11 14:44:56 +0100

    CMake: don't use CMAKE_CXX14_STANDARD_COMPILE_OPTION directly, instead go through CMAKE_CXX_STANDARD

--- cmake/TestStdAny.cpp
+++ cmake/TestStdAny.cpp
@@ -8,9 +8,10 @@
 int main() {
 #if defined(WALBERLA_USE_STD_ANY)
    auto a = std::any(42);
+   std::cout << std::any_cast<int>(a) << std::endl;
 #elif defined(WALBERLA_USE_STD_EXPERIMENTAL_ANY)
    auto a = std::experimental::any(42);
-#endif
    std::cout << std::experimental::any_cast<int>(a) << std::endl;
+#endif
    return 0;
 }
