7fdee2bec6b05a89d9bb78446026ea7732bacfe9
Sebastian Eibl <sebastian.eibl@fau.de>
2020-08-07 15:26:51 +0200

    split raytracing test into smaller junks

--- tests/pe/RaytracingSphere.cpp
+++ tests/pe/RaytracingSphere.cpp
@@ -40,22 +40,49 @@ using namespace walberla::pe::raytracing;
 
 typedef std::tuple<Box, Plane, Sphere, Capsule, Ellipsoid> BodyTuple ;
 
-ShadingParameters customBodyToShadingParams(const BodyID body) {
-   if (body->getID() == 10) {
-      return greenShadingParams(body).makeGlossy(30);
-   } else if (body->getID() == 7) {
-      return greenShadingParams(body).makeGlossy(10);
-   } else if (body->getID() == 9) {
-      return darkGreyShadingParams(body).makeGlossy(50);
-   } else if (body->getID() == 3) {
-      return redShadingParams(body).makeGlossy(200);
-   } else {
-      return defaultBodyTypeDependentShadingParams(body);
+ShadingParameters customSpheresBodyToShadingParams(const BodyID body) {
+   if (body->getTypeID() == Plane::getStaticTypeID()) {
+      return greyShadingParams(body);
+   }
+   
+   switch (body->getID()) {
+      case 0:
+         return blueShadingParams(body).makeGlossy(1);
+      case 1:
+         return blueShadingParams(body).makeGlossy(10);
+      case 2:
+         return blueShadingParams(body).makeGlossy(30);
+      case 3:
+         return blueShadingParams(body).makeGlossy(80);
+      case 4:
+         return whiteShadingParams(body);
+      case 5:
+         return lightGreyShadingParams(body);
+      case 6:
+         return greyShadingParams(body);
+      case 7:
+         return darkGreyShadingParams(body);
+      case 8:
+         return blackShadingParams(body).makeGlossy(100);
+      case 9:
+         return redShadingParams(body);
+      case 10:
+         return blueShadingParams(body);
+      case 11:
+         return violetShadingParams(body);
+      case 12:
+         return greenShadingParams(body);
+      case 13:
+         return greenShadingParams(body).makeGlossy(30);
+      case 14:
+         return blueShadingParams(body).makeGlossy(1000);
+      default:
+         return lightGreyShadingParams(body);
    }
 }
 
-void RaytracerTest(Raytracer::Algorithm raytracingAlgorithm = Raytracer::RAYTRACE_HASHGRIDS, walberla::uint8_t antiAliasFactor = 1) {
-   WALBERLA_LOG_INFO("Raytracer");
+void RaytracerSpheresTestScene(Raytracer::Algorithm raytracingAlgorithm = Raytracer::RAYTRACE_HASHGRIDS, walberla::uint8_t antiAliasFactor = 1) {
+   WALBERLA_LOG_INFO("Raytracer Spheres Scene");
    shared_ptr<BodyStorage> globalBodyStorage = make_shared<BodyStorage>();
    auto forest = blockforest::createBlockForest(AABB(0,0,0,10,10,10), Vector3<uint_t>(1,1,1), Vector3<bool>(false, false, false));
    auto storageID = forest->addBlockData(createStorageDataHandling<BodyTuple>(), "Storage");
@@ -70,17 +97,11 @@ void RaytracerTest(Raytracer::Algorithm raytracingAlgorithm = Raytracer::RAYTRAC
                        real_t(49.13), antiAliasFactor,
                        Vec3(-5,5,5), Vec3(-1,5,5), Vec3(0,0,1), //-5,5,5; -1,5,5
                        lighting,
-                       Color(real_t(0.2), real_t(0.2), real_t(0.2)),
-                       customBodyToShadingParams);
+                       Color(real_t(0.2),real_t(0.2),real_t(0.2)),
+                       customSpheresBodyToShadingParams);
    
    MaterialID iron = Material::find("iron");
    
-   //PlaneID xNegPlane = createPlane(*globalBodyStorage, 0, Vec3(-1,0,0), Vec3(5,0,0), iron);
-   // xNegPlane obstructs only the top left sphere and intersects some objects
-   
-   //PlaneID xNegPlaneClose = createPlane(*globalBodyStorage, 0, Vec3(-1,0,0), Vec3(1,0,0), iron);
-   
-   // Test Scene v1 - Spheres, (rotated) boxes, confining walls, tilted plane in right bottom back corner
    createPlane(*globalBodyStorage, 0, Vec3(0,-1,0), Vec3(0,10,0), iron); // left wall
    createPlane(*globalBodyStorage, 0, Vec3(0,1,0), Vec3(0,0,0), iron); // right wall
    createPlane(*globalBodyStorage, 0, Vec3(0,0,1), Vec3(0,0,0), iron); // floor
@@ -88,35 +109,18 @@ void RaytracerTest(Raytracer::Algorithm raytracingAlgorithm = Raytracer::RAYTRAC
    createPlane(*globalBodyStorage, 0, Vec3(-1,0,0), Vec3(10,0,0), iron); // back wall
    createPlane(*globalBodyStorage, 0, Vec3(1,0,0), Vec3(0,0,0), iron); // front wall, should not get rendered
    
-   createPlane(*globalBodyStorage, 0, Vec3(-1,1,1), Vec3(8,2,2), iron); // tilted plane in right bottom back corner
+   walberla::id_t id=0;
+   for (int j=0; j<4; j++) {
+      for (int i=0; i<4; i++) {
+         createSphere(*globalBodyStorage, *forest, storageID, id, Vec3(6,real_c(i+1)*real_t(2),real_c(j+1)*real_t(2)), real_t(0.9));
+         id++;
+      }
+   }
    
-   createSphere(*globalBodyStorage, *forest, storageID, 2, Vec3(6,real_t(9.5),real_t(9.5)), real_t(0.5));
-   createSphere(*globalBodyStorage, *forest, storageID, 3, Vec3(4,real_t(5.5),5), real_t(1));
-   createSphere(*globalBodyStorage, *forest, storageID, 6, Vec3(3,real_t(8.5),5), real_t(1));
-   BoxID box = createBox(*globalBodyStorage, *forest, storageID, 7, Vec3(5,real_t(6.5),5), Vec3(2,4,3));
-   if (box != nullptr) box->rotate(0,math::pi/4,math::pi/4);
-   createBox(*globalBodyStorage, *forest, storageID, 8, Vec3(5,1,8), Vec3(2,2,2));
-   // Test scene v1 end
-   
-   // Test scene v2 additions start
-   createBox(*globalBodyStorage, *forest, storageID, 9, Vec3(9,9,5), Vec3(1,1,10));
-   createCapsule(*globalBodyStorage, *forest, storageID, 10, Vec3(3, 9, 1), real_t(0.5), real_t(7), iron);
-   CapsuleID capsule = createCapsule(*globalBodyStorage, *forest, storageID, 11, Vec3(7, real_t(3.5), real_t(7.5)), real_t(1), real_t(2), iron);
-   if (capsule != nullptr) capsule->rotate(0,math::pi/3,math::pi/4-math::pi/8);
-   // Test scene v2 end
-   
-   // Test scene v3 additions start
-   EllipsoidID ellipsoid = createEllipsoid(*globalBodyStorage, *forest, storageID, 12, Vec3(6,2,real_t(2.5)), Vec3(3,2,real_t(1.2)));
-   ellipsoid->rotate(0, math::pi/real_t(6), 0);
-   // Test scene v3 end
-   
-   //raytracer.setTBufferOutputDirectory("tbuffer");
-   //raytracer.setTBufferOutputEnabled(true);
    raytracer.setImageOutputEnabled(true);
-   //raytracer.setLocalImageOutputEnabled(true);
    
    raytracer.setRaytracingAlgorithm(raytracingAlgorithm);
-   raytracer.generateImage<BodyTuple>(0);
+   raytracer.generateImage<BodyTuple>(1);
 }
 
 int main( int argc, char** argv )
@@ -128,7 +132,7 @@ int main( int argc, char** argv )
 
    const Raytracer::Algorithm algorithm = Raytracer::RAYTRACE_COMPARE_BOTH_STRICTLY;
    const walberla::uint8_t antiAliasFactor = 1;
-   RaytracerTest(algorithm, antiAliasFactor);
+   RaytracerSpheresTestScene(algorithm, antiAliasFactor);
    
    return EXIT_SUCCESS;
 }
