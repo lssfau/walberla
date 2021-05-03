cd4341a1f79e912a1cc80dc37ad560bb1095420b
Michael Kuron <mkuron@icp.uni-stuttgart.de>
2021-05-03 21:13:08 +0000

    Force use of std::experimental::filesystem with GCC 8

--- cmake/TestStdFilesystem.cpp
+++ cmake/TestStdFilesystem.cpp
@@ -5,6 +5,10 @@
 #include <experimental/filesystem>
 #endif
 
+#if defined(WALBERLA_USE_STD_FILESYSTEM) && defined(__GLIBCXX__) && (!defined(_GLIBCXX_RELEASE) || _GLIBCXX_RELEASE < 9)
+#error "std:filesystem broken due to https://gcc.gnu.org/bugzilla/show_bug.cgi?id=90050"
+#endif
+
 int main() {
 #if defined(WALBERLA_USE_STD_FILESYSTEM)
    std::filesystem::path p("/tmp/test.txt");
