d1d0ae1e71c2b948b7d65e035a4b1ef680214245
Dominik Thoennes <dominik.thoennes@fau.de>
2021-11-29 07:41:00 +0000

    add new MacMini with Arm M1 chip and remove old Macs

--- tests/lbm/codegen/LbmPackInfoGenerationTest.py
+++ tests/lbm/codegen/LbmPackInfoGenerationTest.py
@@ -1,29 +1,30 @@
-from lbmpy.creationfunctions import create_lb_collision_rule, create_lb_update_rule
+import pystencils as ps
+
+from lbmpy.creationfunctions import create_lb_update_rule, LBMConfig, LBMOptimisation
 from lbmpy.advanced_streaming import Timestep
-from lbmpy.stencils import get_stencil
+from lbmpy.enums import Method, Stencil
+from lbmpy.stencils import LBStencil
 from pystencils_walberla import CodeGeneration, generate_pack_info_from_kernel
 from lbmpy_walberla.packinfo import generate_lb_pack_info
 from pystencils.field import Field
 
 with CodeGeneration() as ctx:
+    data_type = "float64" if ctx.double_accuracy else "float32"
     streaming_pattern = 'aa'
-    target = 'cpu'
-    stencil = get_stencil('D3Q19')
-    dim = len(stencil[0])
-    values_per_cell = len(stencil)
-    collision_rule = create_lb_collision_rule(method='srt', stencil=stencil)
-    pdf_field = Field.create_generic('pdfs', dim, index_shape=(values_per_cell,), layout='fzyx')
-    optimization = {
-        'symbolic_field': pdf_field,
-        'target': target
-    }
+    target = ps.Target.CPU
+    stencil = LBStencil(Stencil.D3Q19)
+
+    pdf_field = Field.create_generic('pdfs', stencil.D, index_shape=(stencil.Q,), layout='fzyx')
+
+    lbm_config = LBMConfig(method=Method.SRT, stencil=stencil, streaming_pattern=streaming_pattern,
+                           timestep=Timestep.ODD)
+    lbm_opt = LBMOptimisation(symbolic_field=pdf_field)
 
     #   Generate PackInfo specifically for streaming pattern
     generate_lb_pack_info(ctx, 'AccessorBasedPackInfo', stencil, pdf_field,
                           streaming_pattern=streaming_pattern, target=target, namespace='pystencils')
 
     #   Generate reference using the alternating pull/push approach
-    update_rule_odd = create_lb_update_rule(collision_rule=collision_rule, optimization=optimization,
-                                            streaming_pattern=streaming_pattern, timestep=Timestep.ODD)
+    update_rule_odd = create_lb_update_rule(lbm_config=lbm_config, lbm_optimisation=lbm_opt)
     generate_pack_info_from_kernel(ctx, 'FromKernelPackInfoPull', update_rule_odd, kind='pull', target=target)
     generate_pack_info_from_kernel(ctx, 'FromKernelPackInfoPush', update_rule_odd, kind='push', target=target)
