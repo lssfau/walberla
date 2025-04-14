ee4381aef7457207088fe8ad6c6143ed556a56fb
Frederik Hennig <frederik.hennig@fau.de>
2025-04-14 07:40:41 +0200

    Remove conda deployment tools and CI tasks

--- utilities/conda/walberla/run_test.py
+++
@@ -1,7 +0,0 @@
-import waLBerla
-
-# Check that C++ exports are available
-assert waLBerla.cpp_available
-
-# Test calling of a function taking a string (fails if there is a ABI compatibility issue)
-waLBerla.log_devel("Test successful")
