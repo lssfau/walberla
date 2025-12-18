lssfau/walberla

commit fef704c5b693a9f35315f146ad0bdf15c4196758

Expose SIMD vector size decision tree
Useful to design custom allocators.

src/field/allocation/FieldAllocator.h
@@ -351,6 +351,26 @@ namespace field {
    };
 
 
+constexpr uint_t SIMDAlignment() {
+#if defined(__ARM_FEATURE_SVE) && defined(__ARM_FEATURE_SVE_BITS) && __ARM_FEATURE_SVE_BITS > 0
+   return uint_c(__ARM_FEATURE_SVE_BITS / 8);
+#elif defined(__ARM_FEATURE_SVE)
+   return 64u;
+#elif defined(__ARM_NEON)
+   return 16u;
+#elif defined(__AVX512F__)
+   return 64u;
+#elif defined(__AVX__)
+   return 32u;
+#elif defined(__SSE__) || defined(_MSC_VER)
+   return 16u;
+#elif defined(__BIGGEST_ALIGNMENT__)
+   return uint_c(__BIGGEST_ALIGNMENT__);
+#else
+   return 64u;
+#endif
+}
+
 } // namespace field
 } // namespace walberla
 

src/field/Field.impl.h
@@ -319,23 +319,7 @@ namespace field {
       // Automatically select allocator if none was given
       if ( alloc == nullptr )
       {
-#if defined(__ARM_FEATURE_SVE) && defined(__ARM_FEATURE_SVE_BITS) && __ARM_FEATURE_SVE_BITS > 0
-         const uint_t alignment = __ARM_FEATURE_SVE_BITS/8;
-#elif defined(__ARM_FEATURE_SVE)
-         const uint_t alignment = 64;
-#elif defined(__ARM_NEON)
-         const uint_t alignment = 16;
-#elif defined(__AVX512F__)
-         const uint_t alignment = 64;
-#elif defined(__AVX__)
-         const uint_t alignment = 32;
-#elif defined(__SSE__) || defined(_MSC_VER)
-         const uint_t alignment = 16;
-#elif defined(__BIGGEST_ALIGNMENT__)
-         const uint_t alignment = __BIGGEST_ALIGNMENT__;
-#else
-         const uint_t alignment = 64;
-#endif
+         constexpr uint_t alignment = SIMDAlignment();
 
          // aligned allocator only used (by default) if ...
          if ( l == fzyx                      && // ... we use a structure of arrays layout

commit 3ec4ecb9cfd1496d5b2ceb7330ebb31911ece820

Make SIMDAlignment() truly constexpr
Fixes a regression introduced by fef704c5b693a9f35315f146ad0bdf15c4196758

src/field/allocation/FieldAllocator.h
@@ -353,7 +353,7 @@ namespace field {
 
 constexpr uint_t SIMDAlignment() {
 #if defined(__ARM_FEATURE_SVE) && defined(__ARM_FEATURE_SVE_BITS) && __ARM_FEATURE_SVE_BITS > 0
-   return uint_c(__ARM_FEATURE_SVE_BITS / 8);
+   return static_cast<uint_t>(__ARM_FEATURE_SVE_BITS / 8);
 #elif defined(__ARM_FEATURE_SVE)
    return 64u;
 #elif defined(__ARM_NEON)
@@ -365,7 +365,7 @@ constexpr uint_t SIMDAlignment() {
 #elif defined(__SSE__)
    return 16u;
 #elif defined(__BIGGEST_ALIGNMENT__)
-   return uint_c(__BIGGEST_ALIGNMENT__);
+   return static_cast<uint_t>(__BIGGEST_ALIGNMENT__);
 #else
    return 64u;
 #endif