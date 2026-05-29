0b8bbf868f811f95578e3969f15a23f9c3804798
Michi <michael.zikeli@fau.de>
2026-05-29 11:47:04 +0200

    Remove the addition to FindParmetis. This was a poor attempt to tell CMake that ParMetis is dependent of MPI, but that is not our job and these additions make no sense.

--- cmake/FindParmetis.cmake
+++ cmake/FindParmetis.cmake
@@ -1,19 +1,6 @@
 find_path (PARMETIS_INCLUDE_DIR parmetis.h)
 find_library (PARMETIS_LIBRARY NAMES parmetis)
 
-# ParMetis requires MPI - find MPI as a dependency
-if(NOT Parmetis_FOUND)
-    find_package (MPI REQUIRED)
-    if(MPI_FOUND)
-        if(DEFINED MPI_CXX_INCLUDE_PATH)
-            set (PARMETIS_INCLUDE_DIR
-                 "${PARMETIS_INCLUDE_DIR};${MPI_CXX_INCLUDE_PATH}"
-                 CACHE PATH "ParMetis include directories" FORCE
-            )
-        endif()
-    endif()
-endif()
-
 # handle the QUIETLY and REQUIRED arguments and set PFFT_FOUND to TRUE if
 # all listed variables are TRUE
 include (FindPackageHandleStandardArgs)
